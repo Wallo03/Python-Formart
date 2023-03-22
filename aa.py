9dc7aebce39f4446f3a3f041430785f3f3d2b252
Wallo <wallo@gmail.com>
2023-03-22 09:12:51 +0100

    new file

---
+++ aa.py
@@ -0,0 +1,2 @@
+x = input('i')
+print('ciao'+x)
---
+++ test.py
@@ -0,0 +1,12 @@
+#print("bye")
+from PIL import Image 
+
+img = Image.open("cartella1/japan.jpg")
+
+img.show()
+
+primi = [3,4,5,6,7]
+for pippo in primi[:-3]:
+    print(pippo)
+
+
