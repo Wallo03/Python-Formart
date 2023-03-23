44f11b048c1a51ae66a68f700e993b36b8b04a8b
Wallo <wallo@gmail.com>
2023-03-23 15:04:01 +0100

    update 23/03

---
+++ file.py
@@ -0,0 +1,16 @@
+
+def somma(a:float,b:float):
+    '''QUESTA FUNCTION SOMMA'''
+    s = a+b
+    print(s)
+    return s
+
+
+def main():
+    a=5
+    b=6
+    somma(a,b)
+
+#somma(10,12)
+if __name__ == "__main__": #Va a creare le linee di esecuzione, se va eseguito come esecutore, ogni volta che creero dei file python dovro creare questa struttra qua
+    main()
