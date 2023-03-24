422dcdec86b699105d4c2cb594e439cdef97e6a4
Wallo <wallo@gmail.com>
2023-03-24 09:44:53 +0100

    upt

--- aa.py
+++
@@ -1,2 +0,0 @@
-x = input('i')
-print('ciao'+x)
---
+++ provaMain.py
@@ -0,0 +1,13 @@
+## import dfdfdfdf
+## import fdfd
+
+#def xx():
+#   print('xx)
+
+def main():
+    print('funzia?')
+
+#####################
+
+if __name__ == "__main__":
+    main()
